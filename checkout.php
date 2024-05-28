733328fc93046776157e99c8cb742a820d68813e
Zhou Yehao <azcx8p@inf.elte.hu>
2024-05-29 00:54:13 +0200

    Modified checkout page and header

--- checkout.php
+++ checkout.php
@@ -27,7 +27,7 @@ if (isset($_SESSION['name'])) {
               <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
                 <ul>
              
-                 <li><a href="checkout.php"> Checkout</a></li>
+                 <li><a href="checkout.php"> Check my eSim</a></li>
           
                 </ul>
 
@@ -50,9 +50,9 @@ if (isset($_SESSION['name'])) {
 
         <ol>
           <li><a href="index.php">Home</a></li>
-          <li>checkout</li>
+          <li>Check my eSim</li>
         </ol>
-        <h2>Chechout</h2>
+        <h2>Check my eSim</h2>
 
       </div>
     </section><!-- End Breadcrumbs -->
--- header.php
+++ header.php
@@ -43,11 +43,16 @@
           <li><a class="nav-link scrollto active" href="index.php">Home</a></li>
           <li><a class="nav-link scrollto" href="#about">About</a></li>
     
-          <li><a class="nav-link   scrollto" href="#portfolio">Data Plan</a></li>
+          <li><a class="nav-link   scrollto" href="#pricing">Data Plan</a></li>
           <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+          <li class="dropdown"><a href="#"><span>My Space</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+             
+              <li><a href="Dashboard.php">Dashboad</a></li>
+                  <li><a href="checkout.php">Check my eSim</a></li>
+            </ul>
           </li>
-          <li><a class="nav-link scrollto" href="signup.php">Signup</a></li>
+          <li><a class="nav-link scrollto" href="signup.php">Sign up</a></li>
           <li><a class="getstarted scrollto" href="login.php">Login</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
